e09f20fa30f960b24c76bb27136bb8fc374d275b
dante-kk612 <160244686+dante-kk612@users.noreply.github.com>
2025-10-02 08:09:42 -0300

    Add files via upload

---
+++ dino-site/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", event => {
+    console.log("Service Worler Instalado");
+});
+
+self.addEventListener("actvate", event =>{
+    console.log("Service Worker ativado")
+});
+
+self;addEventListener("fetch", event =>{
+    barabarabara
+});
